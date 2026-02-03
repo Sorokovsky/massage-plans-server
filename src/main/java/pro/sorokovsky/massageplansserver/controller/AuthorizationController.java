6ea34d460007f6e7ec5233cc599d5c08ab173a82
Sorokovsky <Sorokovskys@ukr.net>
2026-02-03 15:05:30 +0200

    feature: all authorization controller documentated

--- src/main/java/pro/sorokovsky/massageplansserver/controller/AuthorizationController.java
+++ src/main/java/pro/sorokovsky/massageplansserver/controller/AuthorizationController.java
@@ -13,12 +13,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import pro.sorokovsky.massageplansserver.contract.GetUserPayload;
+import pro.sorokovsky.massageplansserver.contract.LoginPayload;
 import pro.sorokovsky.massageplansserver.contract.NewUserPayload;
 import pro.sorokovsky.massageplansserver.mapper.UserMapper;
 import pro.sorokovsky.massageplansserver.service.AuthorizationService;
@@ -101,5 +99,80 @@ public class AuthorizationController {
         return ResponseEntity
                 .created(uriBuilder.replacePath("/authorization/profile").build().toUri())
                 .body(mapper.toGet(user));
+    }
+
+    @PostMapping("login")
+    @Operation(
+            description = "Вхід користувача",
+            summary = "Вхід",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(
+                            contentSchema = @Schema(implementation = LoginPayload.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+            )
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    content = @Content(
+                            schema = @Schema(implementation = GetUserPayload.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    ),
+                    responseCode = "200",
+                    description = "Успішний вхід",
+                    headers = {
+                            @Header(
+                                    name = HttpHeaders.AUTHORIZATION,
+                                    schema = @Schema(implementation = String.class),
+                                    required = true,
+                                    description = "Токен доступу",
+                                    example = "Bearer <TOKEN>"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    content = @Content(
+                            schema = @Schema(implementation = ProblemDetail.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    ),
+                    responseCode = "403",
+                    description = "Немає доступу"
+            ),
+            @ApiResponse(
+                    content = @Content(
+                            schema = @Schema(implementation = ProblemDetail.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    ),
+                    responseCode = "400",
+                    description = "Не коректні данні"
+            )
+    })
+    public ResponseEntity<GetUserPayload> login(@Valid @RequestBody LoginPayload payload) {
+        return ResponseEntity.ok(mapper.toGet(service.login(payload)));
+    }
+
+    @DeleteMapping("logout")
+    @Operation(
+            description = "Вихід користувача",
+            summary = "Вихід"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Успішний вихід"
+            ),
+            @ApiResponse(
+                    content = @Content(
+                            schema = @Schema(implementation = ProblemDetail.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    ),
+                    responseCode = "401",
+                    description = "Неавторизований користувач"
+            )
+    })
+    public ResponseEntity<Void> logout() {
+        service.logout();
+        return ResponseEntity.noContent().build();
     }
 }
