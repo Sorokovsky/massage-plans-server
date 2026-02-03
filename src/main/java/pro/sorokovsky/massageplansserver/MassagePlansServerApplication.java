e389e511c5fe9807200f9453f1299d345a56568f
Sorokovsky <Sorokovskys@ukr.net>
2026-02-03 11:44:36 +0200

    initial commit

---
+++ src/main/java/pro/sorokovsky/massageplansserver/MassagePlansServerApplication.java
@@ -0,0 +1,13 @@
+package pro.sorokovsky.massageplansserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassagePlansServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MassagePlansServerApplication.class, args);
+    }
+
+}
